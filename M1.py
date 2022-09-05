69dd33eca610f63448ed09896ce2c0209031da74
bauti <elbautisocial@gmail.com>
2022-09-05 18:16:39 -0300

    Estos son los ejercicios de ob

---
+++ M1.py
@@ -0,0 +1,12 @@
+# Desde la consola de python, debes de imprimir las siguientes cadenas de texto:
+# Hola, soy [tu_nombre]
+# Estoy empezando el curso de python
+# Espero aprender mucho
+
+tu_nombre = str(input('Hola, introduce tu nombre aqui! \n'))
+print (f'Hola, soy {tu_nombre}','estoy empezando el curso de python, espero aprender mucho')
+
+# Desde la consola de python almacena la cadena “Hola mundo!” en una variable y muéstrala.
+
+hola_mundo = 'Hola Mundo!'
+print(hola_mundo)
---
+++ M2.py
@@ -0,0 +1,9 @@
+# En este ejercicio tendrás que crear una variable a la cual asignarás una cadena de texto y la imprimirás por consola, a su vez, tendrás que imprimir también el tipo de la variable por consola.
+
+variable_de_tipo_str = 'Esto es una variable de tipo str'
+
+print(variable_de_tipo_str, type(variable_de_tipo_str))
+
+# Modifica la variable del anterior ejercicio en la consola de python y después muestrala por consola para ver la modificación de la variable.
+variable_de_tipo_str = int(10)
+print(variable_de_tipo_str, type(variable_de_tipo_str))
